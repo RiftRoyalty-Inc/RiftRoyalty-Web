acb2f3a0d7777f95716e048d5bf708a63dd7bc7f
Uchi <a26689@svalero.com>
2024-06-06 17:36:17 +0200

    updated old web with current version of last night

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+function redirigirADsc() {
+    window.location.href = "https://discord.gg/Z6F5cmyt";
+}
+function redirigirADonacion() {
+    window.location.href = "https://ko-fi.com/riftroyaltysi";
+}
+
+function redirigirALoL() {
+    window.location.href = "https://www.leagueoflegends.com/en-us/";
+}
+
+function redirigirPlayStore(){
+    window.location.href = "https://play.google.com/";
+}
